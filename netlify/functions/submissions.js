af31bacb2eae82173763cbcb1f30a4fb797e378d
Charlie Gerard <charlie.a.gerard@gmail.com>
2021-11-18 15:33:24 -0800

    detect Netlify site and send form data to Airtable

--- netlify/functions/submissions.js
+++ netlify/functions/submissions.js
@@ -1,38 +1,46 @@
 const fetch = require("node-fetch");
 const Airtable = require("airtable");
 
-exports.handler = async () => {
+exports.handler = async (event) => {
   const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
   const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 
-  const submission = {
-    Name: "Beep",
-    Email: "beep@boop.com",
-    URL: "http://google.com",
-    "Years unused": 20,
-  };
+  const submission = JSON.parse(event.body);
 
-  var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID);
+  const submissionHeaders = await fetch(submission.URL);
+  const isNetlifySite =
+    new Map(submissionHeaders.headers).get("server") === "Netlify"
+      ? true
+      : false;
 
-  base("Submissions").create(
-    [
-      {
-        fields: submission,
-      },
-    ],
-    function (err, records) {
-      if (err) {
-        console.error(err);
-        return;
+  if (isNetlifySite) {
+    var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(
+      AIRTABLE_BASE_ID
+    );
+    base("Submissions").create(
+      [
+        {
+          fields: submission,
+        },
+      ],
+      function (err, records) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        records.forEach(function (record) {
+          // console.log(record);
+        });
       }
-      records.forEach(function (record) {
-        console.log(record);
-      });
-    }
-  );
-
-  return {
-    statusCode: 200,
-    body: "Hello world",
-  };
+    );
+    return {
+      statusCode: 200,
+      body: "Your site has been successfully submitted! Woop, Woop! 🥳",
+    };
+  } else {
+    return {
+      statusCode: 418,
+      body: "It looks like the site you submitted is not a Netlify site, how dare you. 🙄", //To do: Probably change this to a not-so-aggressive error message 😂
+    };
+  }
 };
