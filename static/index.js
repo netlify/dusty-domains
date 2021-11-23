063d3262ab33f1853a75eff4629051afc61e231d
Lynn Fisher <lynn.fisher@netlify.com>
2021-11-23 10:52:10 -0700

    adjust site lists so they don't overlap on hover

--- static/index.js
+++ static/index.js
@@ -32,3 +32,14 @@ submissionForm.onsubmit = async (e) => {
     }),
   });
 };
+
+/* height for showcase items */
+var showcaseContainer = document.getElementById('showcase');
+var showcaseList = document.getElementById('showcase-list');
+showcaseContainer.style.height = (showcaseList.offsetHeight + 150) + "px";
+showcaseList.setAttribute("style","position:absolute; left:50%; bottom: 0; transform: translateX(-50%);")
+/* height for featured items */
+var featuredContainer = document.getElementById('featured');
+var featuredList = document.getElementById('featured-list');
+featuredContainer.style.height = (featuredList.offsetHeight) + "px";
+featuredList.setAttribute("style","position:absolute; left:50%; bottom: 0; transform: translateX(-50%);")
