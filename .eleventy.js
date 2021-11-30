965bc396cb186d907edfe83fc57dd1a0ab8c447e
Jason Lengstorf <jason@lengstorf.com>
2021-11-29 21:08:12 -0800

    wip: make thanks page dynamic

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,11 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("static");
+const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('static');
+
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: 'share',
+    functionsDir: './netlify/functions/',
+    redirects: 'netlify-toml-builders',
+  });
 };
