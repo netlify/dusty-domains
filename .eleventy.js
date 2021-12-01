25eb3b23377fed51419321adc2d5ce2cc2ce4a43
Jason Lengstorf <jason.lengstorf@netlify.com>
2021-11-30 23:00:37 -0800

    feat: make thanks page dynamic (#37)
    * wip: make thanks page dynamic
    
    * wip: still getting errors
    
    * wip: not generating TOML
    
    * YAMLLLLLLLL Switched from tabs to spaces
    
    * Swap back to ODB.
    
    * Try to get the screenshot service smaller.
    
    * CI
    
    * Try the other bundler.
    
    * change selection colors
    
    * adjust judges grid
    
    * judges grid
    
    * image widths + alts
    
    * generic charity for now
    
    * wip: make thanks page dynamic
    
    * wip: still getting errors
    
    * YAMLLLLLLLL Switched from tabs to spaces
    
    * CI
    
    * Try the other bundler.
    
    * feat: custom OG + page text for submissions
    
    * feat: redirect to customized thanks page after submission
    
    Co-authored-by: Zach Leatherman <zachleatherman@gmail.com>
    Co-authored-by: Lynn Fisher <lynn.fisher@netlify.com>

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,13 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("static");
+const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('static');
+
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: 'thanks',
+    functionsDir: './netlify/functions/',
+    redirects: 'netlify-toml-builders',
+  });
+
+  eleventyConfig.dataFilterSelectors.add('eleventy.serverless.path');
 };
