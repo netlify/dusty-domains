8614c3cbe1c6d57fe9a3830e5f5f0418462fcedf
Prince Wilson <maxcell.wilson@gmail.com>
2021-12-01 20:15:04 -0500

    Revert "Let's try chaining requests"
    This did not solve the issue, chaining can't help
    This reverts commit b1c84eae40ad75b59cb3202a5ea2f8e7b1f1fdb0.

--- static/index.js
+++ static/index.js
@@ -15,27 +15,27 @@ submissionForm.onsubmit = async (e) => {
     'Years unused': parseInt(formData.get('site-date')),
   };
 
-  async function getScreenshotinBytes() {
-    try {
-      const screenshotRes = await fetch('/.netlify/functions/take-screenshot', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-        body: submissionDetails.URL,
-      });
-  
-      return await screenshotRes.text();
-    } catch (e) {
-      document.querySelector('.form-error').textContent =
-        "This site doesn't seem to be deployed on Netlify so we can't accept your submission 😢";
-  
-      console.error(e);
-      formButton.disabled = false;
-    }
+  let screenshotBase64Data;
+
+  try {
+    const screenshotRes = await fetch('/.netlify/functions/take-screenshot', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      body: submissionDetails.URL,
+    });
+
+    screenshotBase64Data = await screenshotRes.text();
+  } catch (e) {
+    document.querySelector('.form-error').textContent =
+      "This site doesn't seem to be deployed on Netlify so we can't accept your submission 😢";
+
+    console.error(e);
+    formButton.disabled = false;
   }
 
-  getScreenshotinBytes().then(async screenshotBase64Data => {
+  if (screenshotBase64Data) {
     try {
       const uploadRes = await fetch('/.netlify/functions/upload', {
         method: 'POST',
@@ -66,7 +66,7 @@ submissionForm.onsubmit = async (e) => {
       console.error(e);
       formButton.disabled = false;
     }
-  });
+  }
 };
 
 /* height for showcase items */
