79195d76ac03010e1a66010343223e90bcf1c393
Jesse Martin <themotleydev@gmail.com>
2021-12-21 21:39:31 +0100

    adding first implementation of thermometer

---
+++ netlify/functions/update.js
@@ -0,0 +1,46 @@
+const fetch = require("node-fetch");
+
+const RATE = 500;
+const GOAL = 100000;
+
+exports.handler = async () => {
+  const query = `query FetchCount {
+        airtable(table: "Submissions") {
+          records
+        }
+      }
+      `;
+
+  let count;
+
+  try {
+    const req = await fetch("https://hasura-b2no.onrender.com/v1/graphql", {
+      method: "POST",
+      body: JSON.stringify({
+        query,
+      }),
+    });
+    const { data } = await req.json();
+
+    if (data?.airtable?.records) {
+      count = data?.airtable?.records.length;
+    } else {
+      count = 0;
+    }
+  } catch (e) {
+    console.log(e.message);
+    return {
+      statusCode: 502,
+      body: JSON.stringify({ message: "Error" }),
+    };
+  }
+
+  const ratio = (count * RATE) / GOAL;
+  const prcntify = (ratio) => `${ratio * 100}%`;
+  const percent = prcntify(ratio);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ percent, ratio, count }),
+  };
+};
--- static/index.js
+++ static/index.js
@@ -1,4 +1,4 @@
-const submissionForm = document.querySelector('.submit-form');
+const submissionForm = document.querySelector(".submit-form");
 
 submissionForm.onsubmit = async (e) => {
   e.preventDefault();
@@ -9,17 +9,17 @@ submissionForm.onsubmit = async (e) => {
   formButton.disabled = true;
 
   const submissionDetails = {
-    Name: formData.get('name'),
-    Email: formData.get('email'),
-    URL: formData.get('url'),
-    'Years unused': parseInt(formData.get('site-date')),
+    Name: formData.get("name"),
+    Email: formData.get("email"),
+    URL: formData.get("url"),
+    "Years unused": parseInt(formData.get("site-date")),
   };
 
   try {
-    const uploadRes = await fetch('/.netlify/functions/upload', {
-      method: 'POST',
+    const uploadRes = await fetch("/.netlify/functions/upload", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         ...submissionDetails,
@@ -34,29 +34,39 @@ submissionForm.onsubmit = async (e) => {
 
       window.location = nextURL;
     } else {
-      throw new Error('Something went wrong while uploading your submission');
+      throw new Error("Something went wrong while uploading your submission");
     }
   } catch (e) {
-    document.querySelector('.form-error').textContent =
-      'Looks like something went a little bit haywire 🤔. Maybe try again!';
+    document.querySelector(".form-error").textContent =
+      "Looks like something went a little bit haywire 🤔. Maybe try again!";
     console.error(e);
     formButton.disabled = false;
   }
 };
 
 /* height for showcase items */
-var showcaseContainer = document.getElementById('showcase');
-var showcaseList = document.getElementById('showcase-list');
-showcaseContainer.style.height = showcaseList.offsetHeight + 150 + 'px';
+var showcaseContainer = document.getElementById("showcase");
+var showcaseList = document.getElementById("showcase-list");
+showcaseContainer.style.height = showcaseList.offsetHeight + 150 + "px";
 showcaseList.setAttribute(
-  'style',
-  'position:absolute; left:50%; bottom: 0; transform: translateX(-50%);',
+  "style",
+  "position:absolute; left:50%; bottom: 0; transform: translateX(-50%);"
 );
 /* height for featured items */
-var featuredContainer = document.getElementById('featured');
-var featuredList = document.getElementById('featured-list');
-featuredContainer.style.height = featuredList.offsetHeight + 'px';
+var featuredContainer = document.getElementById("featured");
+var featuredList = document.getElementById("featured-list");
+featuredContainer.style.height = featuredList.offsetHeight + "px";
 featuredList.setAttribute(
-  'style',
-  'position:absolute; left:50%; bottom: 0; transform: translateX(-50%);',
+  "style",
+  "position:absolute; left:50%; bottom: 0; transform: translateX(-50%);"
 );
+
+// Async call for thermometer height
+(async () => {
+  const thermScale = document.getElementById("therm-scale");
+
+  const req = await fetch("/.netlify/functions/update");
+  const { percent } = await req.json();
+
+  thermScale.style.height = percent || "0%";
+})();
