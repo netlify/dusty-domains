334a2fb53e37372d343b35763b668a8683d953db
Lynn Fisher <lynn.fisher@netlify.com>
2022-01-04 10:48:12 -0700

    remove form, closes #66

--- static/index.js
+++ static/index.js
@@ -1,48 +1,48 @@
-const submissionForm = document.querySelector(".submit-form");
+// const submissionForm = document.querySelector(".submit-form");
 
-submissionForm.onsubmit = async (e) => {
-  e.preventDefault();
-  const formData = new FormData(submissionForm);
+// submissionForm.onsubmit = async (e) => {
+//   e.preventDefault();
+//   const formData = new FormData(submissionForm);
 
-  // Disable submit button to avoid submitting multiple times
-  const formButton = e.submitter;
-  formButton.disabled = true;
+//   // Disable submit button to avoid submitting multiple times
+//   const formButton = e.submitter;
+//   formButton.disabled = true;
 
-  const submissionDetails = {
-    Name: formData.get("name"),
-    Email: formData.get("email"),
-    URL: formData.get("url"),
-    "Years unused": parseInt(formData.get("site-date")),
-  };
+//   const submissionDetails = {
+//     Name: formData.get("name"),
+//     Email: formData.get("email"),
+//     URL: formData.get("url"),
+//     "Years unused": parseInt(formData.get("site-date")),
+//   };
 
-  try {
-    const uploadRes = await fetch("/.netlify/functions/upload", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        ...submissionDetails,
-      }),
-    });
+//   try {
+//     const uploadRes = await fetch("/.netlify/functions/upload", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         ...submissionDetails,
+//       }),
+//     });
 
-    if (uploadRes.ok) {
-      const { redirect } = await uploadRes.json();
+//     if (uploadRes.ok) {
+//       const { redirect } = await uploadRes.json();
 
-      const nextURL = new URL(window.location.href);
-      nextURL.pathname = redirect;
+//       const nextURL = new URL(window.location.href);
+//       nextURL.pathname = redirect;
 
-      window.location = nextURL;
-    } else {
-      throw new Error("Something went wrong while uploading your submission");
-    }
-  } catch (e) {
-    document.querySelector(".form-error").textContent =
-      "Looks like something went a little bit haywire 🤔. Maybe try again!";
-    console.error(e);
-    formButton.disabled = false;
-  }
-};
+//       window.location = nextURL;
+//     } else {
+//       throw new Error("Something went wrong while uploading your submission");
+//     }
+//   } catch (e) {
+//     document.querySelector(".form-error").textContent =
+//       "Looks like something went a little bit haywire 🤔. Maybe try again!";
+//     console.error(e);
+//     formButton.disabled = false;
+//   }
+// };
 
 /* height for showcase items */
 var showcaseContainer = document.getElementById("showcase");
@@ -62,12 +62,12 @@ featuredList.setAttribute(
 );
 
 // Async call for thermometer height
-(async () => {
-  const meter = document.getElementById("progress-bar");
+// (async () => {
+//   const meter = document.getElementById("progress-bar");
 
-  const req = await fetch("/.netlify/functions/update");
-  const { percent } = await req.json();
+//   const req = await fetch("/.netlify/functions/update");
+//   const { percent } = await req.json();
 
-  meter.style.width = percent || "0%";
-  meter.innerText = percent;
-})();
+//   meter.style.width = percent || "0%";
+//   meter.innerText = percent;
+// })();
